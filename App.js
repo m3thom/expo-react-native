aee9669fb1513c63ed6e40ce861c305a068783f8
Mart <thuanmart@nuxos.asia>
2019-11-24 01:19:38 +0700

    prepare to conect to rails backend using redux.

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import reducers from './app/reducers';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const rootReducer = combineReducers({ ...reducers });
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class App extends React.Component {
+  render() {
+    console.log(store.getState())
+    return <Provider store={store}>
+      <View>
+        <Text>Hi there</Text>
+      </View>
+    </Provider>
+  }
+
+};
+
+export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     paddingTop: 10,
+//     backgroundColor: '#ecf0f1',
+//   },
+//   button: {
+//     marginVertical: 10,
+//   },
+// });
